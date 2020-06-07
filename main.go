fc1b1eeaeae669b712582bf5d79fd73128619707
Anton Ptashnik <iavtomator@gmail.com>
2020-06-07 16:40:31 +0300

    refactored - reduced return branches, repetitive comparisons, shortened cursor impl

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "hw3/spiraliterator"
 
 func flatTable(t [][]int) []int {
 	r := make([]int, 0, len(t)*len(t))
-	var i = spiraliterator.New(t)
+	var i = spiraliterator.Create(t)
 	for i.Next() {
 		v := i.Read()
 		r = append(r, v)
--- spiraliterator/iterator.go
+++ spiraliterator/iterator.go
@@ -1,32 +1,27 @@
 package spiraliterator
 
 type cursor struct {
-	col, row  int
-	direction int
+	col, row   int
+	dcol, drow int
 }
 
-func (c *cursor) changeDirection() {
-	c.direction = (c.direction + 1) % (up + 1)
+func (c *cursor) turnRight() {
+	if c.drow != 0 {
+		c.drow *= -1
+	}
+	c.dcol, c.drow = c.drow, c.dcol
 }
 func (c *cursor) move() {
-	switch c.direction {
-	case right:
-		c.col++
-	case down:
-		c.row++
-	case left:
-		c.col--
-	case up:
-		c.row--
-	}
+	c.col += c.dcol
+	c.row += c.drow
 }
 
 // spiralIterator traverses over a table in spiral-like manner
 type spiralIterator struct {
-	c         cursor
-	table     [][]int
-	n         int
-	stepsLeft int
+	c      cursor
+	table  [][]int
+	n      int
+	nSteps int
 }
 
 // Read returns an item the iterator points on
@@ -36,44 +31,33 @@ func (s *spiralIterator) Read() int {
 
 // Next positions the iterator on the next item
 func (s *spiralIterator) Next() bool {
-	if s.n == 0 {
+	if s.nSteps == s.n*s.n {
 		return false
 	}
-	if s.stepsLeft == 0 {
-		if s.n == 1 || (s.n == 2 && s.c.direction+1 == up) {
-			s.n = 0
-			return false
-		}
-		s.c.changeDirection()
-		s.stepsLeft = s.n - 1
-	} else if s.stepsLeft == 1 && s.c.direction == up {
+
+	if s.nSteps > 1 && (s.nSteps-1)%(s.n-1) == 0 {
+		s.c.turnRight()
+	} else if s.nSteps/(s.n-1) == 4 {
+		s.c.turnRight()
 		s.n -= 2
-		if s.n == 0 {
-			return false
-		}
-		s.c.changeDirection()
-		s.stepsLeft = s.n
+		s.nSteps = 0
 	}
 	s.c.move()
-	s.stepsLeft--
+	s.nSteps++
 	return true
 }
 
-const (
-	right = iota
-	down
-	left
-	up
-)
+func Create(t [][]int) spiralIterator {
+	i := spiralIterator{table: t}
+	i.Reset()
+	return i
+}
 
-func New(t [][]int) *spiralIterator {
-	i := spiralIterator{}
-	if len(t) != 0 {
-		i.table = t
+func (i *spiralIterator) Reset() {
+	if len(i.table) != 0 {
 		i.c.col = -1
-		i.n = len(t) + 2
-		i.stepsLeft = 1
-		i.c.direction = up
+		i.n = len(i.table) + 2
+		i.nSteps = (i.n - 1) * 4
+		i.c.drow = -1
 	}
-	return &i
 }
